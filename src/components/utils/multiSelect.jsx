88759ba86a6eef5a2e4252a4f863c52e759ad350
shiv5648 <simranbeatum@gmail.com>
2025-07-10 13:47:33 +0530

    updated

--- src/components/utils/multiSelect.jsx
+++ src/components/utils/multiSelect.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import {
   ChevronDown,
   ChevronRight,
+  ChevronUp,
   ChevronDown as ExpandIcon,
   Search,
 } from "lucide-react";
@@ -18,7 +19,7 @@ export default function FilterDropdownGrouped({
   const [search, setSearch] = useState("");
   const [selectedItems, setSelectedItems] = useState([]);
   const [expandedGroups, setExpandedGroups] = useState({});
-   const ref = useRef(null)
+  const ref = useRef(null)
 
   useEffect(() => {
     setSelectedItems([]);
@@ -44,7 +45,7 @@ export default function FilterDropdownGrouped({
   };
 
   const handleApply = () => {
-    onApply(name,selectedItems);
+    onApply(name, selectedItems);
     setIsOpen(false);
   };
 
@@ -57,7 +58,7 @@ export default function FilterDropdownGrouped({
     name.toLowerCase().includes(search.toLowerCase());
 
 
-   useEffect(() => {
+  useEffect(() => {
     const handleClickOutside = (event) => {
       if (ref.current && !ref.current.contains(event.target)) {
         setIsOpen(false);
@@ -66,7 +67,7 @@ export default function FilterDropdownGrouped({
 
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside); 
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
 
@@ -97,94 +98,104 @@ export default function FilterDropdownGrouped({
           <div className="space-y-2 max-h-40 overflow-y-auto pr-1">
             {!isGrouped
               ? options
-                  .filter((opt) => filterFn(opt.name))
-                  .map((opt) => (
-                    <label
-                      key={opt.value}
-                      className="flex items-center space-x-2"
-                    >
-                      <input
-                        type="checkbox"
-                        checked={selectedItems.includes(opt.value)}
-                        onChange={() => toggleOption(opt.value)}
-                        className="accent-green-600"
-                      />
-                      <span className="text-sm">{opt.name}</span>
-                    </label>
-                  ))
+                .filter((opt) => filterFn(opt.name))
+                .map((opt) => (
+                  <label
+                    key={opt.value}
+                    className="flex items-center space-x-2"
+                  >
+                    <input
+                      type="checkbox"
+                      checked={selectedItems.includes(opt.value)}
+                      onChange={() => toggleOption(opt.value)}
+                      className="accent-green-600"
+                    />
+                    <span className="text-sm">{opt.name}</span>
+                  </label>
+                ))
               : options.map((group) => {
-                  const filteredChildren = group.children.filter((c) =>
-                    filterFn(c.name)
-                  );
+                const filteredChildren = group.children.filter((c) =>
+                  filterFn(c.name)
+                );
 
-                  if (!filterFn(group.name) && filteredChildren.length === 0)
-                    return null;
+                if (!filterFn(group.name) && filteredChildren.length === 0)
+                  return null;
 
-                  const allChecked = group.children.every((c) =>
-                    selectedItems.includes(c.value)
-                  );
+                const allChecked = group.children.every((c) =>
+                  selectedItems.includes(c.value)
+                );
 
-                  return (
-                    <div key={group.value}>
-                      <div
-                        className="flex items-center justify-between cursor-pointer"
-                        onClick={() => toggleGroup(group.value)}
-                      >
-                        <div className="flex items-center space-x-2">
-                          <button type="button">
-                            {expandedGroups[group.value] ? (
-                              <ExpandIcon className="w-4 h-4" />
-                            ) : (
-                              <ChevronRight className="w-4 h-4" />
-                            )}
-                          </button>
-                          <input
-                            type="checkbox"
-                            checked={allChecked}
-                            onChange={() => {
-                              const childValues = group.children.map(
-                                (c) => c.value
-                              );
-                              const areAllSelected = childValues.every((val) =>
-                                selectedItems.includes(val)
-                              );
+                return (
+                  <div key={group.value} className="px-3">
+                    <div
+                      className="flex items-center justify-between gap-2 cursor-pointer"
+                      onClick={() => toggleGroup(group.value)}
+                    >
+                      <div className="flex items-center space-x-2">
+                        <button type="button">
+                          {console.log(expandedGroups[group.value])}
 
-                              setSelectedItems((prev) => {
-                                if (areAllSelected) {
-                                  return prev.filter(
-                                    (val) => !childValues.includes(val)
-                                  );
-                                } else {
-                                  return [...new Set([...prev, ...childValues])];
-                                }
-                              });
-                            }}
-                            className="accent-blue-600"
-                          />
-                          <span className="text-sm font-semibold">
-                            {group.name}
-                          </span>
-                        </div>
+                          {expandedGroups[group.value] ? (
+                            <ChevronUp size={14} className={`font-semibold text-base ${expandedGroups[group.value] ? 'text-black' : 'text-[#6F6F6F]'}`} />
+                          ) : (
+                            <ChevronDown size={14} className={`font-semibold text-base ${expandedGroups[group.value] ? 'text-black' : 'text-[#6F6F6F]'}`} />
+                          )}
+                        </button>
+
+                        <span className={`font-semibold text-base ${expandedGroups[group.value] ? 'text-black' : 'text-[#6F6F6F]'
+                          }`}>
+                          {group.name}
+                        </span>
                       </div>
 
-                      {expandedGroups[group.value] &&
-                        filteredChildren.map((child) => (
+                      <input
+                        type="checkbox"
+
+                        checked={allChecked}
+                        onChange={() => {
+                          const childValues = group.children.map(
+                            (c) => c.value
+                          );
+                          const areAllSelected = childValues.every((val) =>
+                            selectedItems.includes(val)
+                          );
+
+                          setSelectedItems((prev) => {
+                            if (areAllSelected) {
+                              return prev.filter(
+                                (val) => !childValues.includes(val)
+                              );
+                            } else {
+                              return [...new Set([...prev, ...childValues])];
+                            }
+                          });
+                        }}
+                        className="accent-[#00B290]"
+                      />
+                    </div>
+
+
+                    {expandedGroups[group.value] && (
+                      <div className="pl-4 space-y-1 mt-1">
+                        {filteredChildren.map((child) => (
                           <label
                             key={child.value}
-                            className="pl-7 flex items-center space-x-2"
+                           className="flex items-center gap-5 text-base text-[#6F6F6F] font-normal"
                           >
                             <input
                               type="checkbox"
                               checked={selectedItems.includes(child.value)}
                               onChange={() => toggleOption(child.value)}
-                              className="accent-blue-600"
+                              className="accent-[#00B290]"
                             />
-                            <span className="text-sm">{child.name}</span>
+                            <span className="">{child.name}</span>
                           </label>
                         ))}
-                    </div>
-                  );
-                })}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
           </div>
 
           <div className="flex justify-between items-center px-3 py-2">
